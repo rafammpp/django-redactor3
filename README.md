2b5c0371ab2ed591ffabb0836f18849574f8f788
rafa <rafammpp@gmail.com>
2018-05-18 12:37:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Django-redactor3
+Dont download! it's in an early dev state
+
+This not include redactor static files.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django_redactor3',
+      version='0.1',
+      description='Django 2.0 wysiwyg imperavi redactor 3',
+      url='https://github.com/rafammpp/django-redactor3',
+      author='rafammpp',
+      author_email='rafammpp@gmail.com',
+      license='GPL',
+      packages=['redactor'],
+      zip_safe=False)
