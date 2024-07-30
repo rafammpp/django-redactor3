380d44045ced63733fbada868434f956217d5764
rafammpp <rafammpp@gmail.com>
2024-07-30 12:53:51 +0200

    change force_unicode to force_text

--- redactor/utils.py
+++ redactor/utils.py
@@ -1,10 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from importlib import import_module
-
-try:
-    from django.utils.encoding import force_text
-except ImportError:
-    from django.utils.encoding import force_unicode as force_text
+from django.utils.encoding import force_text
 from django.utils.functional import Promise
 
 import json
